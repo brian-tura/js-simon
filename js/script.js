42de5edf02753e803e3fb8b9f832cb5c130de6ef
brian-tura <b.tura2002@gmail.com>
2025-03-28 09:16:21 +0100

    Created countdown

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-const  ul = document.getElementById('numbers-list');
+// Creo i numeri da memorizzare
+const ul = document.getElementById('numbers-list');
 const randomNumbers = [];
 randomNumbers.length = 5;
-for(let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     randomNumbers[i] = Math.floor(Math.random() * 100) + 1;
     const li = document.createElement('li');
     const textNode = document.createTextNode(randomNumbers[i]);
     li.appendChild(textNode);
     ul.appendChild(li);
 }
+// Inizializzo il form
+const form = document.getElementById('answers-form');
+
+// Creo il countdown
+const countdown = document.getElementById('countdown');
+let i = 5;
+showCountDown = setInterval(function () {
+    countdown.innerHTML = i;
+    i--;
+   
+}, 1000);
+
